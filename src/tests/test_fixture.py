e36c6fed02b6966ab54932c72e7ce329eeeb52ad
matthew <matthew@cupist.com>
2020-04-06 21:54:06 +0900

    [modify] 오타 수정

--- src/tests/test_fixture.py
+++ src/tests/test_fixture.py
@@ -7,5 +7,19 @@ def rnd():
     return random.random()
 
 
+@pytest.fixture
+def fixture_a(rnd):
+    return rnd
+
+
+@pytest.fixture
+def fixture_b(rnd):
+    return rnd
+
+
 def test_rnd(rnd):
     print(rnd)
+
+
+def test_multiple(fixture_a, fixture_b):
+    assert fixture_a == fixture_b
