b7451ca37b6696e7dc94f8aca2a25d379d9c2de5
matthew <matthew@cupist.com>
2020-04-06 22:22:08 +0900

    [add] fixture params 추가

--- src/tests/test_fixture.py
+++ src/tests/test_fixture.py
@@ -23,3 +23,30 @@ def test_rnd(rnd):
 
 def test_multiple(fixture_a, fixture_b):
     assert fixture_a == fixture_b
+
+
+@pytest.fixture(params=["ora", "pg", "sqlite"])
+def params(request):
+    return request.param
+
+
+try:
+    import cs_Oracle as ora
+except ImportError:
+    ora = None
+
+needs_ora = pytest.mark.skipif(ora is None, reason="No Oracle installed")
+
+
+@pytest.fixture(params=["pg", needs_ora("ora")])
+def param_two(request):
+    return request.param
+
+
+def test_param(params):
+    assert params == "ora"
+
+
+def test_param_two(param_two):
+    assert param_two is True
+
