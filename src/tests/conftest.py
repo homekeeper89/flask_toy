6d75e3e2b5b59ff8eaf018b522464cc0bb2367d6
matthew <matthew@cupist.com>
2020-04-09 18:22:53 +0900

    [modify] db 환경 새로

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -16,38 +16,3 @@ def flask_app():
 @pytest.fixture(scope="session")
 def flask_client(flask_app):
     return flask_app.test_client()
-@pytest.fixture(scope='session')
-def db(app, request):
-    """Session-wide test database."""
-    if os.path.exists(TESTDB_PATH):
-        os.unlink(TESTDB_PATH)
-
-    def teardown():
-        _db.drop_all()
-        os.unlink(TESTDB_PATH)
-
-    _db.app = app
-    _db.create_all()
-
-    request.addfinalizer(teardown)
-    return _db
-
-
-@pytest.fixture(scope='function')
-def session(db, request):
-    """Creates a new database session for a test."""
-    connection = db.engine.connect()
-    transaction = connection.begin()
-
-    options = dict(bind=connection, binds={})
-    session = db.create_scoped_session(options=options)
-
-    db.session = session
-
-    def teardown():
-        transaction.rollback()
-        connection.close()
-        session.remove()
-
-    request.addfinalizer(teardown)
-    return session
