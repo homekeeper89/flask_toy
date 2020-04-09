aa5800decb1842e1d6ac7c774529baf9bfb48e6e
matthew <matthew@cupist.com>
2020-04-09 22:21:58 +0900

    [add] 테스트용 api 추가

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from src.config import config
+from src.domain import api_main
+
+
+def init_blueprint(app: Flask) -> None:
+    app.register_blueprint(api_main)
 
 
 def create_app(env: str = "dev") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[env])
+    init_blueprint(app)
     return app
