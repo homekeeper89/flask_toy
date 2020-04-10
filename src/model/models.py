c51986c2ecf57a4d4a8369139cba8bea067dbb90
matthew <matthew@cupist.com>
2020-04-10 09:36:31 +0900

    [add] alembic 테스트중 > 뭔가 되고 있다 뭔가가

--- src/model/models.py
+++ src/model/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, DateTime, BigInteger
 
 Base = declarative_base()
 
@@ -7,8 +7,8 @@ Base = declarative_base()
 class Book(Base):
     __tablename__ = "books"
     id = Column(Integer, primary_key=True)
-    title = Column(String)
-    author = Column(String)
+    title = Column(String(50))
+    author = Column(String(50))
     pages = Column(Integer)
     published = Column(Date)
 
@@ -17,3 +17,23 @@ class Book(Base):
             self.title, self.author, self.pages, self.published
         )
 
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(BigInteger, primary_key=True)
+    nickname = Column(String(50))
+    birthday = Column(String(50))
+    created_at = Column("created_at", DateTime)
+    updated_at = Column("updated_at", DateTime)
+
+
+class AuthenticationModel(Base):
+    __tablename__ = "authentications"
+
+    user_id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    category = Column(String(10), primary_key=True)
+    identification = Column(String(50))
+    secret = Column(String(10))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
