67e8dfb348a5cbdc3b79f1e8742dab3383448f6e
matthew <matthew@cupist.com>
2020-04-14 19:15:07 +0900

    [modify] db 추가

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from src import create_app
+from src.database import db as _db
 
 
 @pytest.fixture(scope="session")
 def app():
 
-    app = create_app("testing")
+    app = create_app("test")
     app_context = app.app_context()
     app_context.push()
 
@@ -14,6 +15,39 @@ def app():
     app_context.pop()
 
 
+# noinspection PyShadowingNames
 @pytest.fixture(scope="session")
-def flask_client(app):
+def db(app):
+    with app.app_context():
+        _db.create_all()
+        yield _db
+        _db.drop_all()
+
+
+@pytest.fixture(scope="session")
+def flask_client(app, db):
     return app.test_client()
+
+
+# @pytest.fixture(scope="function")
+# def session(db: SQLAlchemy, request: SubRequest) -> scoped_session:
+#     """
+#     Creates a new persistence session for a tests.
+#     http://alexmic.net/flask-sqlalchemy-pytest/
+#     """
+#     connection = db.engine.connect()
+#     transaction = connection.begin()
+
+#     options = dict(bind=connection, binds={})
+#     session = db.create_scoped_session(options=options)
+
+#     db.session = session
+
+#     def teardown():
+#         transaction.rollback()
+#         connection.close()
+#         session.remove()
+
+#     request.addfinalizer(teardown)
+
+#     return session
--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,15 +1,17 @@
 from src.database import db
-from src.model.models import UserModel
+from src.model.models import User
+import pytest
 
 
 def test_run_app(app):
     assert app is not None
 
 
+@pytest.mark.skip(reason="작업중")
 def test_db_set(session):
     user = {"nickname": "nickanme", "birthday": "nickanme"}
-    session.add(UserModel(**user))
+    session.add(User(**user))
     session.commit()
 
-    res = session.query(UserModel).first()
+    res = session.query(User).first()
     assert res.id != 0
