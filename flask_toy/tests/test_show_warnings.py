e1fa86ffbf5057b0bd983c4f0f8fab56269fd7ae
matthew <homekeeper89@gmail.com>
2020-04-23 01:12:56 +0900

    [add] showing 연습

---
+++ flask_toy/tests/test_show_warnings.py
@@ -0,0 +1,14 @@
+import warnings
+
+def api_v1():
+    warnings.warn(UserWarning("api v1, should use functions from v2"))
+    return 1
+
+def test_one():
+    assert api_v1() == 1
+
+# pytest -q test_show_warnings.py -W error::UserWarning , 에러 나옴
+
+@pytest.mark.filterwarnings("ignore:api v1")
+def test_two():
+    assert api_v1() == 1
