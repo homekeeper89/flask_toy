a483cdc23452df9c6ca5556d9452a4c22074db7a
matthew <homekeeper89@gmail.com>
2020-04-22 23:31:07 +0900

    [add] id 테스트 준비

--- flask_toy/tests/test_flask_toy.py
+++ flask_toy/tests/test_flask_toy.py
@@ -3,3 +3,10 @@ from flask_toy import __version__
 @pytest.mark.version
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize('test', 
+[
+    ('hello')
+])
+def test_id(test):
+    assert test is not None
