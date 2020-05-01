6aa8c191a4db14acd81cd396a19fee8e79fa699d
matthew <matthew@cupist.com>
2020-05-01 18:59:32 +0900

    [modify] swagger 수정

--- src/domain/user/view.py
+++ src/domain/user/view.py
@@ -5,6 +5,7 @@ from flasgger import swag_from
 
 
 @api_user.route("/", methods=["POST"])
+@swag_from("createUser.yml")
 def register():
     data = request.json
     res = UserUseCase().register(data)
@@ -12,8 +13,3 @@ def register():
     res = "success" if res else "fail"
     return jsonify({"data": res})
 
-
-@api_user.route("/", methods=["GET"])
-@swag_from("user.yml")
-def swgger_test():
-    return jsonify("success")
