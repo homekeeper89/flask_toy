be4f2b766d081d38d0d1804e3336ae47b5dfb81d
matthew <matthew@cupist.com>
2020-05-01 14:00:08 +0900

    [add] user-create swagger 형태 추가

--- src/domain/user/view.py
+++ src/domain/user/view.py
@@ -1,6 +1,7 @@
 from src.domain.user import api_user, jsonify
 from src.domain.user.usecase import UserUseCase
 from flask import request
+from flasgger import swag_from
 
 
 @api_user.route("/", methods=["POST"])
@@ -9,5 +10,10 @@ def register():
     res = UserUseCase().register(data)
 
     res = "success" if res else "fail"
-    return jsonify("success")
+    return jsonify({"data": res})
 
+
+@api_user.route("/", methods=["GET"])
+@swag_from("user.yml")
+def swgger_test():
+    return jsonify("success")
