c4388199607a29f5e95600949879fe4ac2667023
matthew <matthew@cupist.com>
2020-05-15 13:53:02 +0900

    [modify] kakao req 테스트 성공

--- src/domain/score/kakao_api_handler.py
+++ src/domain/score/kakao_api_handler.py
@@ -3,14 +3,17 @@ import requests
 
 class KakaoApiHandler:
     KAKAO_HOST = "https://dapi.kakao.com"
-    CATEGORY_SEARCH_API = "/v2/local/search/category.json?category\_group\_code=PM9&radius=20000"
+    CATEGORY_SEARCH_API = "/v2/local/search/category.json?category_group_code=PM9&radius=20000"
 
     def __init__(self, api_key: str):
         self.api_key = api_key
 
     def category_search(self):
+
         api_url = f"{self.KAKAO_HOST}{self.CATEGORY_SEARCH_API}"
-        headers = {"Authorization": f"KaKaoAK {self.api_key}"}
-        res = requests.get(api_url, headers=headers)
-        return res
+        kwargs = dict(
+            method="get", url=api_url, headers={"Authorization": "KakaoAK {}".format(self.api_key)},
+        )
+        resp = requests.request(**kwargs)
+        return resp
 
--- src/tests/basic/test_async.py
+++
@@ -1,2 +0,0 @@
-def test_async():
-    pass
