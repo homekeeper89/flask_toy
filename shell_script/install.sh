612c503893c48768a553459e150c40ec3e5e06bb
matthew <matthew@cupist.com>
2020-05-18 20:38:08 +0900

    [add] shell script 추가, 기본 형태만

--- shell_script/install.sh
+++ shell_script/install.sh
@@ -1,28 +1,15 @@
 #! /bin/bash
 
-version=$(python --version)
-
-echo $version
-
-test_num=5
-
-if ((${test_num} > 3)); then
-    echo "number is greater than 3"
-else
-    echo "number is not greater than 3"
-fi
-
-str1=abc012
-str2=abcd0123
-
-echo "${str1:1}"
-echo "${str2:4}"
-
 ver=$(python -c"import sys; print(sys.version_info.major)")
 
-echo ">>>, ${ver}"
+echo "python version : ${ver}"
 
 if ((${ver} < 3)); then
     echo "python version is low"
-    echo "starting changing python --version to 3"
+    echo "changing python version over 3"
+    echo $(pyenv 3.7.7)
 fi
+
+echo "Set python Env"
+
+$(poetry init)
