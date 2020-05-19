3ea9c560cda379d380471a5509fc44094139a59b
matthew <matthew@cupist.com>
2020-05-19 19:41:04 +0900

    [add] os 테스트 추가중

--- src/tests/test_daum.py
+++ src/tests/test_daum.py
@@ -1,6 +1,12 @@
 from src.domain.score.kakao_api_handler import KakaoApiHandler
+import os
 
 KAKAO_API_KEY = "b53870ce18f5edf1a99e3eae379e0abe"
+
+
+def test_get_os_env():
+    res = os.getenv("score_kakao", False)
+    assert res == False
 
 
 def test_send_api():
