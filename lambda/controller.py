f49b0022d606e1e96a5dd7848606d44903232e39
matthew <homekeeper89@gmail.com>
2020-06-01 23:35:14 +0900

    [modify] cn 애들 삭제, 얘네 뭐지

--- lambda/controller.py
+++ lambda/controller.py
@@ -6,13 +6,11 @@ from flask_api import FlaskAPI, status, exceptions
 from flask_jwt_extended import ( JWTManager, jwt_required, create_access_token, create_refresh_token, get_jwt_identity, jwt_refresh_token_required )
 
 from functools import wraps
-import cn_exception
-import cn_service
 import datetime
 
 app = FlaskAPI(__name__)
 
-app.config['JWT_SECRET_KEY'] = os.environ['cn_jwt_secret_key']
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=10)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = datetime.timedelta(days=15)
@@ -24,8 +22,6 @@ def error_decorator(f):
     def decorated_function(*args, **kwargs):
         try:
             return f(*args, **kwargs)
-        except cn_exception.BizError as be:
-            return jsonify({'code':be.code, 'msg':be.msg}), be.httpcode
         except Exception as ex:
             traceback.print_exc()
             return jsonify({'code':500, 'msg':'Internal Error'}), status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -40,5 +36,7 @@ def ping():
 @error_decorator
 def get_blocked_sites(last):
     account = extract_account(request)
-    result = cn_service.get_blocked_sites(account, last)
-    
+    return jsonify({'code':200, 'msg':'SUCCESS', 'clean_news':result['clean_news'], 'last':result['last'] }), status.HTTP_200_OK
+
+def extract_account(payload):
+    return get_jwt_identity().lower()
