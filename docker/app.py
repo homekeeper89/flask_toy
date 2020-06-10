b37a3aad5f1adc24fa5649c2a331b0320a868b50
matthew <matthew@cupist.com>
2020-06-11 00:39:26 +0900

    [modify] redis가 붙지 않음;

--- docker/app.py
+++ docker/app.py
@@ -4,7 +4,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host="redis", port=6379)
+# cache = redis.Redis(host="redis", port=6379)
 
 
 def get_hit_count():
@@ -21,9 +21,10 @@ def get_hit_count():
 
 @app.route("/")
 def hello():
-    count = get_hit_count()
-    return "Hello World! I have been seen {} times.\n".format(count)
+    # count = get_hit_count()
+    return "Hello World! I have been seen {} times.\n".format(5)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=7000)
+    # app.run()
