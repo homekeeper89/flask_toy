8d1691ccb97912cc6da66376e423655ebeb84f11
matthew <matthew@cupist.com>
2020-06-23 20:20:17 +0900

    [add] 충돌나는 디펜전시 삭제

--- my.py
+++ my.py
@@ -1,24 +1,5 @@
-def greeting(name: str) -> str:
-    return "HELLO" + name
+def add_this(a: int, b: int) -> None:
+    return a + b
 
 
-def p() -> None:
-    print("heel")
-
-
-greeting(3)
-greeting(b"Alice")
-
-from typing import List
-
-
-def greet_all(names: List[str]) -> None:
-    for name in names:
-        print("hell" + name)
-
-
-names = ["Alice", "Bob", "Charile"]
-ages = [10, 20, 30]
-
-kk = input("sdfas")
-greet_all(kk)
+print(add_this(2, 3))
