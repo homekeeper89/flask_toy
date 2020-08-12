146244a46fcc97e519fe60511f9ad0ccea034b02
matthew <matthew@cupist.com>
2020-08-13 00:44:43 +0900

    [modify] feature에 맞게 순서 변경

--- tests/features/test_user.py
+++ tests/features/test_user.py
@@ -7,15 +7,16 @@ scenarios("./user.feature", example_converters=dict(phrase=str))
 
 @given('the app api is queried with "<phrase>"')
 def app_response(phrase):
-    params = {"q": phrase, "format": "json"}
+    params = {"q": phrase, "format": "json", "code": 200}
     return params
+
+
+@then(parsers.parse('the response status code is "{code:d}"'))
+def response_code(app_response, code):
+    assert app_response["code"] == code
 
 
 @then('the response contains results for "<phrase>"')
 def response_contents(app_response, phrase):
     assert phrase.lower() == app_response["q"]
 
-
-@then(parsers.parse('the response status code is "{code:d}"'))
-def response_code(app_response, code):
-    assert app_response["q"] == code
