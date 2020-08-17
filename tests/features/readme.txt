388c225f134a96169871d14d4f75d0f85ce850df
matthew <matthew@cupist.com>
2020-08-17 09:38:18 +0900

    [modify] bdd 관련 테스트 주석 추가

--- tests/features/readme.txt
+++ tests/features/readme.txt
@@ -2,3 +2,4 @@ http://www.datio.com/bbdd/behavior-driver-development-with-python-a-humble-intro
 https://github.com/andresaco/bdd-salt-shaker/blob/master/step_2/features/serving.feature
 
 https://jenisys.github.io/behave.example/tutorials/tutorial01.html
+- behave 모듈에 관한 내용
--- tests/step_defs/test_example.py
+++ tests/step_defs/test_example.py
@@ -4,25 +4,25 @@ from pytest_bdd import scenario, given, when, then, parsers
 
 @scenario("../features/arguments.feature", "Arguments for given, when, thens")
 def test_arguments():
-    print("i am test")
+    print("i am test")  # 제일 마지막에 실행
     pass
 
 
 @given(parsers.parse("there are {start:d} cucumbers"), target_fixture="start_cucumbers")
 def start_cucumbers(start):
-    print("start cucumber")
+    print("start cucumber")  # 첫번째로 실행
     return dict(start=start, eat=0)
 
 
 @when(parsers.parse("I eat {eat:d} cucumbers"))
 def eat_cucumbers(start_cucumbers, eat):
-    print("eat cucumber")
+    print("eat cucumber")  # 두번째로 실행
     start_cucumbers["eat"] += eat
 
 
 @then(parsers.parse("I should have {left:d} cucumbers"))
 def should_have_left_cucumbers(start_cucumbers, start, left):
-    print("left cucumber")
+    print("left cucumber")  # 세번째로 실행
     assert start_cucumbers["start"] == start
     assert start - start_cucumbers["eat"] == left
 
