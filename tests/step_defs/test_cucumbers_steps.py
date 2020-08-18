dcc62e40e448a56a4f6596c92dccbde447cd3f17
matthew <matthew@cupist.com>
2020-08-18 23:21:44 +0900

    [modify] extra_types 변경

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -4,6 +4,8 @@ from src.bdd.cucumber import CucumberBasket
 
 scenarios("../features/cucumbers.feature")
 
+EXTRA_TYPES = {"Number": int}
+
 # @scenario("../features/cucumbers.feature", "Add cucumbers to a basket")
 # def test_add():
 #     pass
@@ -14,14 +16,12 @@ scenarios("../features/cucumbers.feature")
 #     pass
 
 
-@given(parsers.parse("the basket has {initial:Number} cucumbers", extra_types=dict(Number=int)))
+@given(parsers.parse("the basket has {initial:Number} cucumbers", extra_types=EXTRA_TYPES))
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
-@when(
-    parsers.parse("{some:Number} cucumbers are added to the basket", extra_types=dict(Number=int))
-)
+@when(parsers.parse("{some:Number} cucumbers are added to the basket", extra_types=EXTRA_TYPES))
 def add_cucumbers(basket, some):
     basket.add(some)
 
