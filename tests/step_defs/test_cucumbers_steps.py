5a6e2557c5c86d26dd171d8d4a60ba5fba7a88a4
matthew <matthew@cupist.com>
2020-08-20 00:36:16 +0900

    [add] examples 추가
    해당 기능을 사용하려면 step에 추가 deco가 필요함

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,9 +1,13 @@
 from functools import partial
+from tests.step_defs.test_serving import CONVERTERS
 from pytest_bdd import scenario, given, when, then, parsers, scenarios
 
 from src.bdd.cucumber import CucumberBasket
 
-scenarios("../features/cucumbers.feature")
+
+CONVERTERS = {"initial": int, "some": int, "total": int}
+
+scenarios("../features/cucumbers.feature", example_converters=CONVERTERS)
 
 EXTRA_TYPES = {"Number": int}
 parse_num = partial(parsers.parse, extra_types=EXTRA_TYPES)
@@ -18,20 +22,25 @@ parse_num = partial(parsers.parse, extra_types=EXTRA_TYPES)
 
 
 @given(parse_num("the basket has {initial:Number} cucumbers"))
+@given("the basket has <initial> cucumbers")  # examples시 필요
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
 @when(parsers.parse("{some:Number} cucumbers are added to the basket", extra_types=EXTRA_TYPES))
+@when("<some> cucumbers are added to the basket")
 def add_cucumbers(basket, some):
+    print(some)
     basket.add(some)
 
 
 @when(parsers.parse("{some:d} cucumbers are removed from the basket"))
+@when("<some> cucumbers are removed from the basket")
 def remove(basket, some):
     basket.remove(some)
 
 
 @then(parsers.parse("the basket contains {total:Number} cucumbers", extra_types=dict(Number=int)))
+@then("the basket contains <total> cucumbers")
 def basket_has_total(basket, total):
     assert basket.count == total
