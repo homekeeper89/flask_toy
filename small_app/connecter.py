35dd90f1fcab4ab5e2be19303dddbd798782bb8d
matthew <matthew@cupist.com>
2020-12-08 23:01:31 +0900

    [modify] 기본 셀레니움 세팅

--- small_app/connecter.py
+++ small_app/connecter.py
@@ -1,10 +1,22 @@
+from selenium import webdriver
 import requests
 
 
 class Connecter:
+    WAITTING_SECONDS = 15
+
     def __init__(self, target_url):
         self.target_url = target_url
 
     def get(self):
         res = requests.get(self.target_url)
         return res
+
+    def execute(self):
+        driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+        driver.implicitly_wait(self.WAITTING_SECONDS)
+
+        driver.get(self.target_url)
+
+        return driver.quit()
+
--- tests/small_app/test_auto.py
+++ tests/small_app/test_auto.py
@@ -2,8 +2,8 @@ from small_app.connecter import Connecter
 
 
 def test_auth_connect_should_go_homepage():
-    url = "https://jsonplaceholder.typicode.com/posts/1"
+    url = "https://soomgo.com/"
     obj = Connecter(url)
 
-    res = obj.get()
+    res = obj.execute()
     assert res.status_code == 200
