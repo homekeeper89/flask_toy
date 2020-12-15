bf2d474acb054607f01363b994cf3aa13e3a87eb
matthew <matthew@cupist.com>
2020-12-15 21:01:46 +0900

    [modify] 로그인 관련 환경 정보 추가 위해 수정

--- small_app/connecter.py
+++ small_app/connecter.py
@@ -1,7 +1,9 @@
 import string
 from selenium import webdriver
-import requests
 import selenium
+
+from konfig import Config
+import os
 
 
 class Connecter:
@@ -10,6 +12,7 @@ class Connecter:
     def __init__(self, target_url):
         self.target_url = target_url
         self.driver: selenium = "chrome"
+        self.user_info = Config(os.path.join(os.getcwd(), "small_app/") + "conf.ini")
 
     @property
     def driver(self):
@@ -22,7 +25,16 @@ class Connecter:
             self.__driver.implicitly_wait(self.WAITTING_SECONDS)
         print(f"{value} driver is ready")
 
-    def get_elem_by_xpath(xpath: string):
+    @property
+    def user_info(self):
+        return self.__user_info
+
+    @user_info.setter
+    def user_info(self, value):
+        user = value.get_map("user")
+        self.__user_info = user
+
+    def get_elem_by_xpath(self, xpath: string):
         try:
             return self.driver.find_element_by_xpath(xpath)
         except Exception as e:
@@ -45,11 +57,11 @@ class Connecter:
     def type_email_password(self):
         email_xpath = '//*[@id="__BVID__229"]'
         email_input = self.get_elem_by_xpath(email_xpath)
-        email_input.send_keys("kkk")
+        email_input.send_keys(self.__user_info.get("email", "empty"))
 
         pwd_xpath = '//*[@id="__BVID__231"]'
         pwd_input = self.get_elem_by_xpath(pwd_xpath)
-        pwd_input.send_keys("kk")
+        pwd_input.send_keys(self.__user_info.get("password", "pwd"))
 
         login_submit_elem = self.get_elem_by_xpath(
             '//*[@id="app-body"]/div/div/form/div/div[4]/button'
