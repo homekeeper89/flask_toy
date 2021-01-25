4b9271bef6121f95b2f3a4852520da4fa9924e74
matthew <matthew@cupist.com>
2021-01-25 21:18:50 +0900

    [modify] 기본 정보들은 모두 처음에 받는 식으로 변경

--- small_app/base/base_worker.py
+++ small_app/base/base_worker.py
@@ -1,12 +1,17 @@
 import os
 from abc import ABCMeta, abstractmethod
+from selenium import webdriver
+
 from konfig import Config
-from dataclass.user_data import UserConfigData
+from dataclass.user_data import UserConfigData, FilterData
 
 
 class BaseWorker(metaclass=ABCMeta):
-    def __init__(self, conf: str = None):
+    def __init__(self, conf: str = None, filter_data: FilterData = None):
         self.conf = conf
+        self.__driver = None
+        self.user_data: UserConfigData = None
+        self.filter_data = filter_data
 
     @property
     def conf(self):
@@ -18,6 +23,13 @@ class BaseWorker(metaclass=ABCMeta):
         if user_info:
             self.user_data = UserConfigData(**user_info)
             print(self.user_data)
+            self.set_driver(self.user_data.driver)
+
+    def set_driver(self, value):
+        if value == "chrome":
+            self.__driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+            self.__driver.implicitly_wait(self.user_data.waiting_seconds)
+        print(f"{value} driver is ready")
 
     # @abstractmethod
     # def go_to_login(self):
@@ -25,10 +37,6 @@ class BaseWorker(metaclass=ABCMeta):
 
     # @abstractmethod
     # def enter_information(self):
-    #     pass
-
-    # @abstractmethod
-    # def set_driver(self):
     #     pass
 
     # @abstractmethod
--- small_app/dataclass/user_data.py
+++ small_app/dataclass/user_data.py
@@ -8,6 +8,7 @@ class UserConfigData(BaseModel):
     password: str
     driver: str
     waiting_seconds: Optional[int] = 15
+    is_delete_mode: bool = True
 
     @validator("driver")
     def is_driver_in_list(cls, value: str):
@@ -15,3 +16,8 @@ class UserConfigData(BaseModel):
         if value not in basic_driver:
             raise ValueError("지원하지 않는 드라이버")
         return value
+
+
+class FilterData(BaseModel):
+    need_words: List[str] = [""]
+    ben_words: List[str] = [""]
