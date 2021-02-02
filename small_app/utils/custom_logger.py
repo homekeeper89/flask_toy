8ff68098dd1364daf9b6962948f62f733ef7c621
matthew <matthew@cupist.com>
2021-02-02 22:17:58 +0900

    [modify] 기본 file method 추가

--- small_app/utils/custom_logger.py
+++ small_app/utils/custom_logger.py
@@ -1,7 +1,22 @@
+import os
+import datetime
+from pathlib import Path
+
+
 class CustomLogger:
     def __init__(self, category: str):
         self.category = category
 
-    def make_log(self, path: str, contents: str):
+    def make_log(self, contents: dict):
         if self.category == "file":
             return True
+        now = datetime.datetime.now().strftime("%Y_%m_%d")
+        file_path = os.getcwd() + "/small_app/log/" + now
+        Path(file_path).mkdir(parents=True, exist_ok=True)
+
+        state = contents.get("state", "delete")
+        message = contents.get("message", "")
+
+        today = f"{file_path}/[{state}]"
+        with open(today, "a") as file:
+            file.write(message + "\n")
